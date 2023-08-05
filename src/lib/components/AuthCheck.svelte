469c7ce883b6b3cf7c7e7b31f4ecdf38a94a2546
cagrikutayok <cagrikutayok@hotmail.com>
2023-08-05 13:28:11 +0300

    sveltekit practice finished

---
+++ src/lib/components/AuthCheck.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    import { user } from "$lib/firebase";
+</script>
+
+{#if $user}
+    <slot />
+{:else}
+    <h2 class="card-title">You are not logged in</h2>
+    <a href="/login" class="text-center text-error">Please sign in to continue</a>
+{/if}
